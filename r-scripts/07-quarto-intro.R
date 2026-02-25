fc18034a49e54b246cac68383daae35fea828ffd
juewu-wj <juewu0221@gmail.com>
2026-02-25 12:38:22 -0500

    Built site for gh-pages

--- r-scripts/07-quarto-intro.R
+++ r-scripts/07-quarto-intro.R
@@ -46,15 +46,6 @@ data |>
 
 patch
 
-# save(list = c("my_object"),
-#      file = "results.Rdata")
-
-# if(file.exists("results.Rdata")) {
-#   load("results.Rdata")
-# } else {
-#   source("a-really-long-script.R")
-# }
-
 # install.packages("gtsummary")
 # library(gtsummary)
 
